eef7aca16b82a5db4f0c428e1b3f3ad8233f45cc
Subodh Sharma <subodh.v.sharma@gmail.com>
2014-03-13 16:46:52 +0000

    cardinality files

--- solver/solver-src/sat/cardinality.h
+++ solver/solver-src/sat/cardinality.h
@@ -8,172 +8,108 @@
 #define CPROVER_PROP_CARDINALITY_H
 
 
-#include <solver-src/prop/prop.h>
-//#include <literal.h>
+#include "prop.h"
+#include "literal.h"
 #include <vector>
 #include <list>
 #include<algorithm>
-#include <iostream>
+#include <set>
+#include <iterator>
+#ifdef DEBUG
+#include<iostream>
+#endif
 
 typedef std::list<bvt> formulat;
 class  encodingt
 {
-	protected :
-		//reference to prop must be passed so that valid auxiliary variables
-		// can be created
-		 propt& prop;
-public :
-		 encodingt(propt& _prop):prop(_prop){}
+  protected :
+    //reference to prop must be passed so that valid auxiliary variables
+    // can be created
+    propt& prop;
+  public :
+    encodingt(propt& _prop):prop(_prop){}
 
-  /***************************************************************************\
-   * Function : add_to_prop
-   *
-   * Input : prop, formula
-   *
-   * Output : prop
-   *
-   * Purpose : given a list of clause/bvt/vector of literals add it to the
-   * solver prop
-   *
-   * This separate method is provided so that it does not force people to add
-   * cardinality constraint/formula in the solver if they do not wish. All
-   * other encoding methods just returns the formula without adding to a solver
-   */
-  virtual void add_to_prop(formulat& formula)
-  {
-    for(formulat::iterator it=formula.begin();
-        it!=formula.end();it++)
+    virtual void add_to_prop(formulat& formula);
+    virtual literalt get_lit_for_formula(formulat& formula);
+    virtual literalt get_enabling_lit_for_formula(formulat& formula);
+    /*****************************************************************************\
+     * Function : encodingt::sanity_check
+     *
+     * Input : literals, k, formula
+     *
+     * OUtput : true/false
+     *
+     * Purpose : Ensure that set os literals are not empty or k!=0
+     * Also the literals provided should all come from different variables
+     * ***************************************************************************/
+    virtual bool sanity_check(const bvt& literals,const unsigned int k,formulat& formula)
     {
-      prop.lcnf((*it));
-    }
-  }
+      unsigned int size=literals.size();
+      formula.clear();
+      if(size==0 || k==0) return false;
 
-  virtual void print_formula(std::ostream& out,formulat& formula)
-  {
-	  for(formulat::iterator it=formula.begin();
-			  it!=formula.end();it++)
-	  {
-		  for(bvt::iterator bit=it->begin();
-				  bit!=it->end();bit++)
-		  {
-			out << bit->dimacs() << " ";
-		  }
-		  out << std::endl;
-	  }
-  }
+      std::set<unsigned int> tmpset;
 
-  virtual ~encodingt() {}
-  /***************************************************************************\
-   * Function : atmostk
-   *
-   * Input : literals, k
-   *
-   * Output : formula, true/false
-   *
-   * Purpose : encode the constraint <=k(literals) in formula
-   *
-   * Note that the input is a vector of literals (not variables). This means
-   * for a vector (\neg a b \neg c) and k=1 the formula returned enforces that
-   * at most one of the following is true (a==false,b==true,c==false) for a
-   * satisfying assignment of the formula
-   *
-   * formulat is a cnf formula (list of vector of literals = conjunction of
-   * disjunction of literals = conjunction of clauses)
-   *
-   * return valus is FALSE if improper input is given such as empty
-   * set of literals
-   */
-  virtual bool atmostk(const bvt& literals,unsigned int k,formulat& formula)=0;
-
-
-  virtual bool atmostone(const bvt& literals,formulat& formula)
-  {
-    if(literals.empty()) return false;
-    return atmostk(literals,1,formula);
-  }
-
-  virtual bool atleastk(const bvt& literals,const unsigned int k,formulat& formula)
-  {
-    if(literals.empty()) return false;
-    formula.clear();
-    unsigned int size = literals.size();
-    if(size==k)
-    {
-      // if size==k it means that all the literals should be satisfied
-      // so just AND them together
       for(bvt::const_iterator it=literals.begin();it!=literals.end();it++)
       {
-        bvt clause;
-        clause.push_back(*it);
-        formula.push_back(clause);
-      }
+        //given set of literals should all belong to different variables
+        //if a variable repeats then return false  
+        if(!tmpset.insert(it->var_no()).second) return false;
+      }  
       return true;
     }
-
-    else if(size > k)
+#ifdef DEBUG
+    virtual void print_formula(std::ostream& out,formulat& formula)
     {
+      for(formulat::iterator it=formula.begin();
+          it!=formula.end();it++)
+      {
+        for(bvt::iterator bit=it->begin();
+            bit!=it->end();bit++)
+        {
+          out << bit->dimacs() << " ";
+        }
+        out << std::endl;
+      }
+    }
+#endif
 
-    bvt nliterals;
-    for(bvt::const_iterator it=literals.begin();it!=literals.end();it++)
+    virtual ~encodingt() {}
+    /***************************************************************************\
+     * Function : atmostk
+     *
+     * Input : literals, k
+     *
+     * Output : formula, true/false
+     *
+     * Purpose : encode the constraint <=k(literals) in formula
+     *
+     * Note that the input is a vector of literals (not variables). This means
+     * for a vector (\neg a b \neg c) and k=1 the formula returned enforces that
+     * at most one of the following is true (a==false,b==true,c==false) for a
+     * satisfying assignment of the formula
+     *
+     * formulat is a cnf formula (list of vector of literals = conjunction of
+     * disjunction of literals = conjunction of clauses)
+     *
+     * return valus is FALSE if improper input is given such as empty
+     * set of literals
+     */
+    virtual bool atmostk(const bvt& literals,
+        unsigned int k,formulat& formula)=0;
+
+
+    virtual bool atmostone(const bvt& literals,formulat& formula)
     {
-      nliterals.push_back(!(*it));
+      if(!sanity_check(literals,1,formula)) return false;
+      return atmostk(literals,1,formula);
     }
+    virtual bool atleastk(const bvt& literals,
+        const unsigned int k,formulat& formula);
 
-     return atmostk(nliterals,size-k,formula);
+    virtual bool atleastone(const bvt& literals,formulat&formula);
 
-    }
-    else
-    {
-      formula.clear();
-      return false;
-    }
-  }
-
-
-
-  virtual bool atleastone(const bvt& literals,formulat&formula)
-  {
-    if(literals.empty()) return false;
-    formula.clear();
-    bvt clause;
-    for(bvt::const_iterator it=literals.begin();it!=literals.end();it++)
-    {
-      clause.push_back(*it);
-    }
-     formula.push_back(clause);
-     return true;
-
-  }
-
-  virtual bool exactlyk(const bvt& literals,const unsigned int k,formulat& formula)
-  {
-    if(literals.empty()) return false;
-
-    unsigned int size=literals.size();
-
-    if(size==k)
-    {
-      return atleastk(literals,k,formula);
-    }
-    else if(k > size)
-    {
-      formula.clear();
-      return false;
-    }
-
-    bool result = false;
-
-    formulat atmostformula,atleastformula;
-
-    result = atmostk(literals,k,atmostformula);
-    result = result && atleastk(literals,k,atleastformula);
-
-    formula.splice(formula.begin(),atmostformula);
-    formula.splice(formula.begin(),atleastformula);
-
-    return result;
-
-  }
+    virtual bool exactlyk(const bvt& literals,const unsigned int k,formulat& formula);
 
 };
 
@@ -181,27 +117,52 @@ public :
 
 class commander_encodingt : public encodingt
 {
-public:
-	commander_encodingt(propt& _prop):encodingt(_prop){}
-  virtual bool atmostk(const bvt& literals,unsigned int k,formulat& formula);
-protected:
-  virtual bool atmostk(const bvt& literals,
-      unsigned int k, formulat& formula, unsigned int gsize);
+  public:
+    commander_encodingt(propt& _prop):encodingt(_prop){}
+    virtual bool atmostk(const bvt& literals,unsigned int k,
+        formulat& formula);
+  protected:
+    virtual bool atmostk(const bvt& literals,
+        unsigned int k, formulat& formula, unsigned int gsize);
 
 };
 
 class binomial_encodingt : public encodingt
 {
-public:
-binomial_encodingt(propt& _prop):encodingt(_prop){}
-  virtual bool atmostk(const bvt& literals,unsigned int k,formulat& formula);
-  virtual bool atmostone(const bvt& literals,formulat& formula);
+  public:
+    binomial_encodingt(propt& _prop):encodingt(_prop){}
+    virtual bool atmostk(const bvt& literals,unsigned int k,
+        formulat& formula);
+    virtual bool atmostone(const bvt& literals,formulat& formula);
 
 };
 
-template <class T>
+
+class sequential_encodingt : public encodingt
+{
+  public:
+    sequential_encodingt(propt& _prop):encodingt(_prop){}
+    virtual bool atmostk(const bvt& literals,unsigned int k,
+        formulat& formula);
+};
+
+/*****************************************************************************\
+ * Function : subsets_k
+ *
+ * Input : v,k,curr_set
+ *
+ * OUtput : subsets
+ *
+ * Purpose : Given the universe v, size k and curr_set
+ * add to subsets if we have constructed a subset of v of size k
+ *
+ * otherwise use recursive back-tracking to construct curr_set with
+ * different elements of v one-by-one
+ * ***************************************************************************/
+  template <class T>
 void subsets_k(const std::vector<T>& v, const unsigned int k,
-    std::list<std::list<T> >& subsets, std::list<T>& curr_set,const unsigned int index)
+    std::list<std::list<T> >& subsets, std::list<T>& curr_set,
+    const unsigned int index)
 {
   if(k==0)
   {
@@ -229,7 +190,7 @@ void subsets_k(const std::vector<T>& v, const unsigned int k,
  * Purpose : given a set v and size k, in subsets return all subsets of v
  * of size k
  */
-template <class T>
+  template <class T>
 void subsets_k(const std::vector<T>& v,const unsigned int k,
     std::list<std::list<T> >& subsets)
 {
@@ -243,8 +204,8 @@ void subsets_k(const std::vector<T>& v,const unsigned int k,
     subsets.clear();
     return;
   }
-   std::list<T> curr_set;
-   subsets_k(v,k,subsets,curr_set,0);
+  std::list<T> curr_set;
+  subsets_k(v,k,subsets,curr_set,0);
 }
 
 #endif
